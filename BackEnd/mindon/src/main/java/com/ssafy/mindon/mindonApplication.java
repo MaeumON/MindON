ba2662c74d8a259b39f00332bb17ae5f0d7785f2
sooinji <suin0413@naver.com>
2025-01-22 16:21:57 +0900

    BE Initial Setting

---
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/mindonApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy.mindon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mindonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(mindonApplication.class, args);
+    }
+}
