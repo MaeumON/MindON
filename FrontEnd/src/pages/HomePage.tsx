face7f2ab12511a63ce573990a265ca943a3ac5a
lha0 <lhy23456@gmail.com>
2025-01-25 20:13:00 +0900

    Style: color 및 기본 레이아웃 설정 - 오프화이트, 주요색상 설정 - container 적용 (center: true) - 스크린 사이즈 : sm = 360px이하, md = 412px S12P11B103-176

--- FrontEnd/src/pages/HomePage.tsx
+++ FrontEnd/src/pages/HomePage.tsx
@@ -15,13 +15,20 @@ function HomePage() {
     fetchGroups();
   }, []);
   return (
-    <>
-      <div className="text-3xl font-jamsilRegular">잠실 레귤러</div>
-      <div className="text-3xl font-jamsilMedium">잠실 미디엄</div>
-      <div className="text-3xl font-jamsilBold">잠실 볼드</div>
-      <div className="text-3xl font-suite ">스위트</div>
+    <div className="container m-auto sm:max-w-360 sm:bg-yellow100 md:max-w-412 md:bg-green100 ">
+      <div className="text-3xl font-jamsilRegular text-cardTitle">잠실 레귤러 & Card Title</div>
+      <div className="text-3xl font-jamsilMedium text-cardLongContent">잠실 미디엄 & Card Long Content</div>
+      <div className="text-3xl font-jamsilBold text-cardContent">잠실 볼드 & Card Content</div>
+      <div className="text-3xl font-suite text-cardSubcontent"> 스위트 & Card Subcontent</div>
       <div className="text-3xl font-suite font-bold">스위트 볼드</div>
-    </>
+
+      <div className="bg-yellow100 w-20 h-20 text-center">Primary Yellow</div>
+      <div className="bg-green100 w-20 h-20 text-center">Primary Green</div>
+      <div className="bg-orange100 w-20 h-20 text-center">Primary Orange</div>
+      <div className="bg-skyBlue100 w-20 h-20 text-center">Primary Sky Blue</div>
+      <div className="bg-red100 w-20 h-20 text-center">Primary Red</div>
+      <div className="bg-offWhite w-20 h-20 text-center">Off White</div>
+    </div>
   );
 }
 
