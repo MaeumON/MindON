6d2db73e907fe06c42dd00d11cda22b8c7d2e82f
hyunhee <jhc03032@naver.com>
2025-01-31 23:32:05 +0900

    Feat : FindIdFinish Page UI 구현 -FindIdFinish Page UI 구현 -Wrapper, Form 공통 컴포넌트 구현 - S12P11B103-193

--- FrontEnd/src/pages/auth/FindIdFinish.tsx
+++ FrontEnd/src/pages/auth/FindIdFinish.tsx
@@ -1,26 +1,20 @@
-import InputBox from "@/components/common/InputBox";
 import Button from "@/components/common/Button";
 
 function FindIdFinish() {
+  const userId: string = "ssafy103";
   return (
-    <div className="font-suite flex flex-col items-center justify-center w-full h-full px-[20px] gap-[30px]">
-      <div className="flex flex-col font-suite font-bold justify-start gap-[60px] w-full">
+    <div className="font-suite flex flex-col items-center justify-center w-full h-screen px-[20px] gap-3">
+      <div className="flex flex-col font-bold gap-[60px] w-full text-center">
         <section className="text-2xl ">
-          <div>기존에 가입한 계정의</div>
-          <div>이름과 이메일을 입력해 주세요.</div>
+          <div>가입한 계정의 아이디는</div>
+          <div>{userId}</div>
+          <div>입니다.</div>
         </section>
-        <section className="flex flex-col gap-[30px]">
-          <div className="flex flex-col gap-3">
-            <div className="text-xl">이름</div>
-            <InputBox text={"이름"} />
-          </div>
-          <div className="flex flex-col gap-3">
-            <div className="text-xl">이메일</div>
-            <InputBox text={"이메일"} />
-          </div>
-        </section>
-        <Button text={"아이디 찾기"} type={"GREEN"} />
+        <Button text={"로그인하기"} type={"GREEN"} />
       </div>
+      <section className="flex justify-center text-[16px] text-cardContent font-medium">
+        <p>비밀번호를 잊어버리셨나요?</p>
+      </section>
     </div>
   );
 }
