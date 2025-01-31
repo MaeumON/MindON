4ef47f20079419c812e6594f1905c0e0904cb2e0
hyunhee <jhc03032@naver.com>
2025-01-31 19:08:00 +0900

    Feat:ModifyPwd page UI 구현 - FindIdFinsih page와 ModifyPwd 페이지 라우팅 - ModifyPwd page UI 구현 - S12P11B103-197

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -6,6 +6,8 @@ import Login from "@/pages/auth/Login";
 import SignUp from "@/pages/auth/SignUp";
 import FindId from "@/pages/auth/FindId";
 import FindPwd from "@/pages/auth/FindPwd";
+import ModifyPwd from "@/pages/auth/ModifyPwd";
+import FindIdFinish from "@/pages/auth/FindIdFinish";
 import Main from "./pages/Main";
 
 function App() {
@@ -17,7 +19,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/findid" element={<FindId />} />
+      <Route path="/findidfinish" element={<FindIdFinish />} />
       <Route path="/findpwd" element={<FindPwd />} />
+      <Route path="/modifypwd" element={<ModifyPwd />} />
       <Route path="/main" element={<Main />} />
     </Routes>
   );
