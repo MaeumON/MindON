563422dc08a6373d514f9b138fd79a4c1eab1728
dkgus4012 <dkgus4012@naver.com>
2025-02-02 00:45:54 +0900

    Test: nginx 프론트엔드 연결 Test

--- FrontEnd/src/mocks/browser.ts
+++ FrontEnd/src/mocks/browser.ts
@@ -1,4 +1,7 @@
+/* 도커파일 테스트---------------
 import { setupWorker } from "msw/browser";
 import handlers from "./handlers";
 
 export const worker = setupWorker(...handlers);
+
+==========================*/
--- FrontEnd/src/mocks/server.ts
+++ FrontEnd/src/mocks/server.ts
@@ -1,4 +1,7 @@
+/* 도커파일 테스트---------------
 import { setupServer } from "msw/node";
 import handlers from "./handlers";
 
 export const server = setupServer(...handlers);
+
+==========================*/
