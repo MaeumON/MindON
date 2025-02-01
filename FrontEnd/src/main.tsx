86b0ff3a22094eb4d6463754036ec975128c5021
dkgus4012 <dkgus4012@naver.com>
2025-02-02 01:37:43 +0900

    Test: Frontend, nginx 연결 Test

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+# import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -7,20 +7,20 @@ import { BrowserRouter } from "react-router-dom";
 import { worker } from "./mocks/browser";
 ==========================*/
 
-if (process.env.NODE_ENV === "development") {
-  //worker.start().then(() => {
-    createRoot(document.getElementById("root")!).render(
-      <StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StrictMode>
-    );
-  //});
-} else {
+// if (process.env.NODE_ENV === "development") {
+//   //worker.start().then(() => {
+//     createRoot(document.getElementById("root")!).render(
+//   #    <StrictMode>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </StrictMode>
+//     );
+//   //});
+// } else {
   createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
   );
-}
+// }
