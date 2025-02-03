6203900e0e220573e86de64ecc2968720b312e94
류현 <ryoo918@naver.com>
2025-02-03 16:55:17 +0900

    Fix: 타입스크립트 코드 에러 수정 - shadowcard : reactnode, cssproperties 추가가 - diseaseNAme : string 설정

--- FrontEnd/src/apis/groupApi.ts
+++ FrontEnd/src/apis/groupApi.ts
@@ -8,13 +8,13 @@ export const fetchGroups = async (): Promise<groupType[]> => {
     // API 응답 구조 확인
     console.log("API 응답:", response);
 
-    // 응답이 예상과 다를 경우 예외 처리
-    if (!response.data || typeof response.data !== "object" || !("data" in response.data)) {
-      console.error("Invalid API response format:", response);
-      return [];
-    }
+    // // 응답이 예상과 다를 경우 예외 처리
+    // if (!response.data || typeof response.data !== "object" || !("data" in response.data)) {
+    //   console.error("Invalid API response format:", response);
+    //   return [];
+    // }
 
-    return response.data.data;
+    return response.data;
   } catch (error) {
     console.error("Error fetching groups:", error);
     return [];
--- FrontEnd/src/components/common/ShadowCard.tsx
+++ FrontEnd/src/components/common/ShadowCard.tsx
@@ -1,4 +1,12 @@
-const Card = ({ children, className = "", style = {} }) => {
+import { ReactNode, CSSProperties } from "react";
+
+interface CardProps {
+  children: ReactNode; // children은 ReactNode 타입
+  className?: string; // className은 선택적인 string 타입
+  style?: CSSProperties; // style은 선택적인 CSSProperties 타입
+}
+
+const Card = ({ children, className = "", style = {} }: CardProps) => {
   return (
     <section className={`flex flex-col justify-center ${className}`} style={style}>
       <section className="bg-white rounded-[12px] shadow-md p-5 w-[330px]">
