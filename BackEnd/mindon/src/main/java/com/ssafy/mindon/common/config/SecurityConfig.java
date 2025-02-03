fcaacf9faa4f58fd9556d349982e2c1c0a5157d7
dkgus4012 <dkgus4012@naver.com>
2025-02-03 23:45:46 +0900

    Test: 프론트엔드 백엔드 통신 test

--- BackEnd/mindon/src/main/java/com/ssafy/mindon/common/config/SecurityConfig.java
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/common/config/SecurityConfig.java
@@ -6,6 +6,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 백엔드 프론트엔드 통신 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
+
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         http
+//                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+//                 .authorizeRequests()
+//                 .requestMatchers("/**").permitAll()  // 모든 URL 요청에 대해 접근을 허용, 후에 수정 필요
+//                 .and();
+//         return http.build();
+//     }
+// }
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,9 +34,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeRequests()
-                .requestMatchers("/**").permitAll()  // 모든 URL 요청에 대해 접근을 허용, 후에 수정 필요
+                .requestMatchers("/**").permitAll()
                 .and();
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://i12b103.p.ssafy.io");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
