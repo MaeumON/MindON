03121e4d53e6b25af0b957717bd62cfbf3952dc3
류현 <ryoo918@naver.com>
2025-02-04 12:30:52 +0900

    Design :  메인페이지 캐러셀 적용, 하단바 구현 - 메인페이지 카드 컴포넌트 캐러셀 적용 - 하단바 footer 생성 S12P11B103-232

--- FrontEnd/src/components/common/ShadowCard.tsx
+++ FrontEnd/src/components/common/ShadowCard.tsx
@@ -4,11 +4,18 @@ interface CardProps {
   children: ReactNode; // children은 ReactNode 타입
   className?: string; // className은 선택적인 string 타입
   style?: CSSProperties; // style은 선택적인 CSSProperties 타입
+  onClick?: () => void;
 }
 
-const Card = ({ children, className = "", style = {} }: CardProps) => {
+const Card = ({ children, className = "", style = {}, onClick }: CardProps) => {
   return (
-    <section className={`flex flex-col justify-center ${className}`} style={style}>
+    <section
+      className={`flex flex-col justify-center ${className} ${onClick ? "cursor-pointer" : ""}`}
+      style={style}
+      onClick={onClick}
+      role={onClick ? "button" : undefined} // ✅ 접근성 추가
+      tabIndex={onClick ? 0 : undefined}
+    >
       <section className="bg-white rounded-[12px] shadow-md p-5 w-[330px]">
         <div className="flex flex-col gap-[10px]">{children}</div>
       </section>
