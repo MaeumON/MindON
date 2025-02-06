bd754d6d975b8e5beca1f309338b36240eacc052
lha0 <lhy23456@gmail.com>
2025-02-07 08:47:23 +0900

    Chore: api 주소 도메인으로 변경경

--- FrontEnd/src/apis/openvidu/questionApi.ts
+++ FrontEnd/src/apis/openvidu/questionApi.ts
@@ -2,7 +2,7 @@ import instance from "../instance";
 import { questionType } from "../types/questions";
 
 //meetingID에 맞는 질문 받아오기
-export const fetchQuestions = async (meetingId: number): Promise<questionType[]> => {
+export const fetchQuestions = async (meetingId: string): Promise<questionType[]> => {
   const response = await instance.get(`/api/meetings/${meetingId}/questions`);
 
   return await response.data;
