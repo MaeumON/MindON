2c58170e13580c80e1534117e4dff463396ff050
sooinji <suin0413@naver.com>
2025-02-07 15:27:47 +0900

    Feat: 실시간 모임 녹음 API 구현 - 녹음 시작 API 구현 - 녹음 종료 API 구현(종료 -> 로컬 저장 -> 삭제) S12P11B103-235

--- BackEnd/mindon/src/main/java/com/ssafy/mindon/video/service/VideoService.java
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/video/service/VideoService.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.ssafy.mindon.group.entity.Group;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -43,9 +47,12 @@ public class VideoService {
     public String initializeSession(Map<String, Object> params) throws Exception {
         try {
             String groupId = (String) params.get("customSessionId");
-            System.out.println(params);
             SessionProperties properties = SessionProperties.fromJson(params).build();
             Session session = openvidu.createSession(properties);
+//            // 세션에 연결된 모든 참가자의 연결 정보를 가져오는 코드
+//            List<Connection> connections = session.getConnections();
+//            System.out.println("세션 정보: " + session);
+//            System.out.println("연결 정보: " + connections);  // 참가자(연결) 정보 출력
             return session.getSessionId();
         } catch (OpenViduJavaClientException | OpenViduHttpException e) {
             throw new Exception("Error initializing session: " + e.getMessage());
@@ -71,8 +78,12 @@ public class VideoService {
             Recording.OutputMode outputMode = Recording.OutputMode.valueOf((String) params.get("outputMode"));
             boolean hasAudio = (boolean) params.get("hasAudio");
             boolean hasVideo = (boolean) params.get("hasVideo");
+            String uuid = UUID.randomUUID().toString().replaceAll("-", ""); // UUID에서 "-" 제거
+            String randomString = uuid.substring(0, 5); // 앞 5자리만 사용
+            System.out.println(randomString);
 
             RecordingProperties properties = new RecordingProperties.Builder()
+                    .name(randomString)
                     .outputMode(outputMode)
                     .hasAudio(hasAudio)
                     .hasVideo(hasVideo)
@@ -124,6 +135,32 @@ public class VideoService {
                     .orElse(false); // 그룹이 없으면 기본값 false 반환
         } catch (NumberFormatException e) {
             return false; // sessionId가 숫자가 아닐 경우 false 반환
+        }
+    }
+
+    public void saveRecording(String url, String sessionId, String userId, int questionId) {
+        try {
+            // URL에서 파일 다운로드
+            URL fileUrl = new URL(url);
+            InputStream in = fileUrl.openStream();
+
+            // 확장자 추출
+            String fileExtension = url.substring(url.lastIndexOf("."));
+
+            // 로컬 저장 경로 설정 (C:/recordings)
+            String directory = "C://recordings/";
+            String fileName = sessionId + "_" + userId + "_" + questionId + fileExtension;
+            Path filePath = Paths.get(directory, fileName);
+
+            // 디렉토리 존재하지 않으면 생성
+            Files.createDirectories(Paths.get(directory));
+
+            // 파일 저장
+            Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
+            in.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
