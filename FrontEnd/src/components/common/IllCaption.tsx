b200ea3685e331cc5738a1f6d5d967a6c60d6942
류현 <ryoo918@naver.com>
2025-02-10 17:12:14 +0900

    style: illcaption 자동 너비 수정 완료

--- FrontEnd/src/components/common/IllCaption.tsx
+++ FrontEnd/src/components/common/IllCaption.tsx
@@ -4,7 +4,7 @@ interface IllCaptionProps {
 
 const IllCaption = ({ diseaseName }: IllCaptionProps) => {
   return (
-    <div className="h-[35px] text-center bg-white rounded-[16px] border-[1px] border-solid border-cardSubcontent font-suite text-18px font-[700] text-cardContent">
+    <div className="inline-flex items-center justify-center w-auto max-w-fit px-3 py-1 text-center bg-white rounded-[16px] border-[1px] border-solid border-cardSubcontent font-suite text-18px font-[700] text-cardContent">
       {diseaseName}
     </div>
   );
