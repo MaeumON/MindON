e29f0270d2ab344368356f3b9f0a067a075994a5
류현 <ryoo918@naver.com>
2025-02-10 21:51:52 +0900

    feat:마이데이터 상세 상단 차트 구현 완료

---
+++ FrontEnd/src/apis/group/topFiveGroupsApi.ts
@@ -0,0 +1,15 @@
+import { Group } from "./groupListApi";
+import authInstance from "../authinstance";
+
+const topFiveGroupsApi = async (diseaseId: number): Promise<Group[]> => {
+  try {
+    const result = await authInstance.get<Group[]>(`/api/groups/${diseaseId}/list`);
+    console.log("📌 전체 API 응답:", result);
+    return result.data;
+  } catch (error) {
+    console.error("groupList axios error:", error);
+    throw error;
+  }
+};
+
+export default topFiveGroupsApi;
