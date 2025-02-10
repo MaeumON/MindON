02b186f8d310a73f1b46442270332e5ee2853e2e
sooinji <suin0413@naver.com>
2025-02-10 15:23:31 +0900

    Chore: 예외 처리 추가 - 예외 처리 형식, 핸들러 추가 S12P11B103-298

---
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/common/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.ssafy.mindon.common.exception;
+
+import com.ssafy.mindon.common.error.ErrorCode;
+
+public class NotFoundException extends BusinessBaseException {
+
+    public NotFoundException(ErrorCode errorCode) {
+        super(errorCode.getMessage(), errorCode);
+    }
+
+    public NotFoundException() {
+        super(ErrorCode.NOT_FOUND);
+    }
+}
