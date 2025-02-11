cbd0625349686e89035e8b42225f687b6762eb20
sooinji <suin0413@naver.com>
2025-02-11 12:44:40 +0900

    Feat: 회원 신고 API 구현 - 회원 신고 기능 구현 S12P11B103-300

---
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/report/repository/ReasonRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.mindon.report.repository;
+
+import com.ssafy.mindon.report.entity.Reason;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReasonRepository extends JpaRepository<Reason, Byte> {
+}
