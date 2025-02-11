e917223482a4bc682e7438e8cdd0a95db51eadd1
dkgus4012 <dkgus4012@naver.com>
2025-02-11 20:51:59 +0900

    Fix: 감정온도 api temperature 수정

---
+++ BackEnd/mindon/src/main/java/com/ssafy/mindon/user/dto/UserEmotionResponseDto.java
@@ -0,0 +1,9 @@
+package com.ssafy.mindon.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+public class UserEmotionResponseDto {
+    private int temperature;  // 감정 평균
+}
