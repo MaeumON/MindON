829e180491b068d1f4eea5d3212ab642cd79d915
lha0 <lhy23456@gmail.com>
2025-02-13 08:51:01 +0900

    Fix: authInstance 내 Access Control Origin 옵션 추가

--- FrontEnd/src/apis/authinstance.ts
+++ FrontEnd/src/apis/authinstance.ts
@@ -7,6 +7,7 @@ const authInstance = axios.create({
   baseURL: VITE_APP_API_URL, // 프로덕션 환경
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
   },
   withCredentials: true,
 });
