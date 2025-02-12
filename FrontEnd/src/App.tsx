cca6b1e3695ad89e74d2bd43b72b504682b17202
hyunhee <jhc03032@naver.com>
2025-02-12 23:23:07 +0900

    Feat : UpdateUser UI 및 기능 구현 - UpdateUser UI 구현 - API 생성 및 연결 - S12P11B103-350

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -15,6 +15,7 @@ import MyDataList from "@pages/MyDataList";
 import MyPage from "@pages/mypage/MyPage";
 import MypageDetail from "@pages/mypage/MypageDetail";
 import MyPageGroupList from "@pages/mypage/MyPageGroupList";
+import UpdateUser from "./pages/auth/UpdateUser";
 
 const queryClient = new QueryClient();
 
@@ -22,13 +23,23 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
+        <Route path="/main" element={<Main />} />
+
+        {/* openvidu */}
         <Route path="/prejoin/:groupId/:groupName" element={<PreJoin />} />
+
+        {/* auth */}
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/findid" element={<FindId />} />
         <Route path="/findpwd" element={<FindPwd />} />
-        <Route path="/main" element={<Main />} />
+
+        {/* group */}
+        <Route path="/groupslist" element={<GroupsList />} />
+        <Route path="/groups/:groupId" element={<GroupDetail />} />
+
+        {/* mydata */}
         <Route path="/mydata" element={<MyData />} />
         <Route path="/mydata/grouplist/:groupStatus" element={<MyDataList />} />
         <Route
@@ -36,12 +47,12 @@ function App() {
           element={<div className="font-jamsilMedium text-32px flex justify-center my-10">잘못된 주소입니다!</div>}
         />
         <Route path="/mydata/:groupId" element={<MyDataDetail />} />
-        <Route path="/groupslist" element={<GroupsList />} />
-        <Route path="/groups/:groupId" element={<GroupDetail />} />
 
+        {/* mypage */}
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/mypage/grouplist/:groupStatus" element={<MyPageGroupList />} />
         <Route path="/mypage/:groupId" element={<MypageDetail />} />
+        <Route path="/mypage/updateuser" element={<UpdateUser />} />
       </Routes>
     </QueryClientProvider>
   );
--- FrontEnd/src/components/common/InputForm.tsx
+++ FrontEnd/src/components/common/InputForm.tsx
@@ -8,6 +8,7 @@ interface InputBoxProps {
   type?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
 }
 
 const InputForm = ({
@@ -18,16 +19,19 @@ const InputForm = ({
   type = "text",
   value,
   onChange,
+  disabled = false,
 }: InputBoxProps) => {
   return (
     <div className="flex flex-col gap-3">
       <div className={`${titleClassName}`}>{title}</div>
       <input
         type={type}
-        className={`grow shrink basis-0 font-suite rounded-xl py-3 px-4 text-cardLongContent bg-White text-lg justify-start items-center font-bold whitespace-nowrap w-full outline-none border-2 focus:border-yellow100 focus:border-yellow100  focus:ring-0.5 focus:ring-yellow100 ${className}`}
+        className={`grow shrink basis-0 font-suite rounded-xl py-3 px-4 text-cardLongContent
+          disabled:text-cardContent disabled:bg-cardSubcontent disabled:cursor-not-allowed  bg-White text-lg justify-start items-center font-bold whitespace-nowrap w-full outline-none border-2 focus:border-yellow100 focus:border-yellow100  focus:ring-0.5 focus:ring-yellow100 ${className} `}
         placeholder={holder}
         value={value}
         onChange={onChange}
+        disabled={disabled}
       ></input>
     </div>
   );
