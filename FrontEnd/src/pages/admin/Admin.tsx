9c8a5509b8c26537e614722d153f73d0d412c347
lha0 <lhy23456@gmail.com>
2025-02-14 19:54:29 +0900

    Fix: 백엔드 데이터 적용

--- FrontEnd/src/pages/admin/Admin.tsx
+++ FrontEnd/src/pages/admin/Admin.tsx
@@ -1,23 +1,8 @@
 import { getReportedUserApi, ReportedUser, unblockUserApi } from "@/apis/auth/adminApi";
-import useLogoutApi from "@/apis/auth/logoutApi";
-import Button from "@/components/common/Button";
-import Header from "@/components/Layout/Header";
+import useLogoutApi from "@apis/auth/logoutApi";
+import Button from "@components/common/Button";
+import Header from "@components/Layout/Header";
 import { useEffect, useState } from "react";
-
-const data = [
-  {
-    userId: "1",
-    userName: "홍길동",
-  },
-  {
-    userId: "2",
-    userName: "이순신",
-  },
-  {
-    userId: "3",
-    userName: "강감찬",
-  },
-];
 
 const Admin = () => {
   const onClickLogout = useLogoutApi();
@@ -48,10 +33,7 @@ const Admin = () => {
       <div className="w-full min-h-screen px-[20px] mt-[30px] font-suite">
         <span className="ml-[5px] text-20px font-bold">신고 유저 목록</span>
         <ul className="h-[calc(100vh-160px-80px)] mt-[20px] flex flex-col gap-[20px] overflow-y-auto">
-          {/* {reportedUsers.map((user) => (
-            <li key={user.userId}>{user.userName}</li>
-          ))} */}
-          {data.map((user) => (
+          {reportedUsers.map((user) => (
             <div
               key={user.userId}
               className="px-[20px] py-[10px] flex justify-between items-center bg-white rounded-[12px] shadow-lg"
